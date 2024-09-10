464a0526d403dc3797a09ad20e5d234f8a817862
Levi Nicola <138073857+RedFoX1029@users.noreply.github.com>
2024-09-10 20:33:41 -0300

    Add: 2920482411030.java

---
+++ FazerVsCode/src/2920482411030.java
@@ -0,0 +1,18 @@
+public class 2920482411030 {
+
+  static String nome = "Levi Nicola Santos";
+  static int rgm = 2920482411030;
+  static int idade = 19;
+  static String gitHub = "https://github.com/RedFoX1029";
+  static String linkedin = "https://www.linkedin.com/in/levi-nicolas-803037258/";
+  static String cursoTurma = "Analise e Desenvolvimento de Sistemas - 2º Semestre";
+
+  public static void main(String[] args) {
+    System.out.println("RGM do aluno: " + rgm);
+    System.out.println("Nome completo do aluno: " + nome);
+    System.out.println("Idade do aluno: " + idade);
+    System.out.println("Perfil do github: " + gitHub);
+    System.out.println("Perfil do linkedin: " + linkedin);
+    System.out.println("Curso e turma do aluno: " + cursoTurma); 	
+  }
+}
